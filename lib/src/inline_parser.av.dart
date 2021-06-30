0764c7f7badaf6a47436a2dd041b22705bc72933
roubachof <jm.alfonsi@gmail.com>
2021-06-30 19:29:55 +0200

    added various secrets syntaxes

--- lib/src/inline_parser.av.dart
+++ lib/src/inline_parser.av.dart
@@ -4,36 +4,84 @@ part of 'inline_parser.dart';
 ///////////////////// ENTERING ARCADE VALLEY MARKDOWN ////////////////////////
 //////////////////////////////////////////////////////////////////////////////
 
-class AudioSyntax extends LinkSyntax {
-  AudioSyntax({Resolver? linkResolver})
-      : super(linkResolver: linkResolver, pattern: r'!a\[', startCharacter: $exclamation);
+class ArcadeValleyLinkSyntax extends LinkSyntax {
+  final String elementName;
+
+  ArcadeValleyLinkSyntax(this.elementName, String pattern,
+      {Resolver? linkResolver})
+      : super(
+            linkResolver: linkResolver,
+            pattern: pattern,
+            startCharacter: $exclamation);
 
   @override
-  Element _createNode(String destination, String? title, {required List<Node> Function() getChildren}) {
-    var element = Element.empty('audio');
+  Element _createNode(String destination, String? title,
+      {required List<Node> Function() getChildren}) {
+    var element = Element.empty(elementName);
     var children = getChildren();
     element.attributes['src'] = destination;
-    element.attributes['id'] = children.map((node) => node.textContent).join();
+    String idWithPoints = children.map((node) => node.textContent).join();
+    var splitIdPoints = idWithPoints.split(':');
+    element.attributes['id'] = splitIdPoints[0];
+    element.attributes['points'] = splitIdPoints[1];
     if (title != null && title.isNotEmpty) {
-      element.attributes['title'] = escapeAttribute(title.replaceAll('&', '&amp;'));
+      element.attributes['title'] =
+          escapeAttribute(title.replaceAll('&', '&amp;'));
     }
     return element;
   }
 }
 
-class NavigationSyntax extends LinkSyntax {
-  NavigationSyntax({Resolver? linkResolver})
-      : super(linkResolver: linkResolver, pattern: r'!aud\[', startCharacter: $exclamation);
+class AudioSyntax extends ArcadeValleyLinkSyntax {
+  AudioSyntax({Resolver? linkResolver})
+      : super(
+          'audio',
+          r'!aud\[',
+          linkResolver: linkResolver,
+        );
+}
 
-  @override
-  Element _createNode(String destination, String? title, {required List<Node> Function() getChildren}) {
-    var element = Element.empty('audio');
-    var children = getChildren();
-    element.attributes['src'] = destination;
-    element.attributes['id'] = children.map((node) => node.textContent).join();
-    if (title != null && title.isNotEmpty) {
-      element.attributes['title'] = escapeAttribute(title.replaceAll('&', '&amp;'));
-    }
-    return element;
-  }
+class MusicSyntax extends ArcadeValleyLinkSyntax {
+  MusicSyntax({Resolver? linkResolver})
+      : super(
+          'music',
+          r'!mus\[',
+          linkResolver: linkResolver,
+        );
+}
+
+class PodcastSyntax extends ArcadeValleyLinkSyntax {
+  PodcastSyntax({Resolver? linkResolver})
+      : super(
+          'podcast',
+          r'!pod\[',
+          linkResolver: linkResolver,
+        );
+}
+
+class NavigationSyntax extends ArcadeValleyLinkSyntax {
+  NavigationSyntax({Resolver? linkResolver})
+      : super(
+          'navigation',
+          r'!nav\[',
+          linkResolver: linkResolver,
+        );
+}
+
+class IfSyntax extends ArcadeValleyLinkSyntax {
+  IfSyntax({Resolver? linkResolver})
+      : super(
+          'if',
+          r'!if\[',
+          linkResolver: linkResolver,
+        );
+}
+
+class ThenSyntax extends ArcadeValleyLinkSyntax {
+  ThenSyntax({Resolver? linkResolver})
+      : super(
+          'then',
+          r'!then\[',
+          linkResolver: linkResolver,
+        );
 }
